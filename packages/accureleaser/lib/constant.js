12ed412bcefade53eb1c71cbdbf6bcbf63185839
E0SelmY4V <yabo1887415157@qq.com>
2024-04-05 21:18:17 +0800

    Refactor(accureleaser): 删了promisify
    node已经有promisify了

--- packages/accureleaser/lib/constant.js
+++ packages/accureleaser/lib/constant.js
@@ -5,19 +5,3 @@ import safe from './safe-do-fn.js';
 
 export const myDir = path.join(getDirname(import.meta.url), '..');
 export const myPackageJSON = JSON.parse(`${await safe.myPackageJSON(fsp.readFile, [path.join(myDir, 'package.json')])}`);
-/**
- * 把需要回调的函数包装成 Promise
- * @template {any[]} P
- * @template T
- * @param {(...args: [...P, (err: any, data: T) => void]) => any} fn 要被包装的函数
- * @returns {(...args: P) => Promise<T>}
- */
-export function promisify(fn) {
-	return (...args) => {
-		return new Promise((res, rej) => {
-			/**@type {[...P, (err: any, data: T) => void]} */
-			const argAll = [...args, (err, data) => (err ? rej(err) : res(data))];
-			fn(...argAll);
-		});
-	};
-}
